788646a112a8c49364dc19f760487eb3ecc08dc0
Tiancheng Lin <tiancheng@cambly.com>
2024-04-10 11:53:07 +0800

    For demo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useEffect, useState } from 'react'
+
+const random = Math.random
 
 function App() {
+  const [emojis, setEmojis] = useState([])
+  useEffect(() => {
+    if (emojis.length > 10) {
+      return
+    }
+    setTimeout(() => {
+      const size = random() * 0.75 + 1
+      setEmojis([
+        ...emojis,
+        {
+          animationDelay: `${random() * 3 + 2}s`,
+          width: `${size}em`,
+          height: `${size}em`,
+          left: `${random() * 120}px`,
+          bottom: `${random() * 20}px`,
+        },
+      ])
+    }, random() * 200)
+  }, [emojis])
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +31,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+      {emojis.map((e, i) => (
+        <img key={i} className="emoji" style={e} src="/512.gif" alt="emoji" />
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
